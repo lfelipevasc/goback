de33c97da48cdfff9f0236ac23ece03ffbae791f
Luis Felipe <lfelipevasconcellos@hotmail.com>
2020-12-28 16:33:05 -0300

    scheduling creation

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -17,11 +17,12 @@ describe('CreateAppointment', ()=>{
     it('should be able to create a new appointment',  async () =>{
     const appointment = await createAppointment.execute({
             date: new Date(),
-            provider_id: '11234',
+            user_id: '123123',
+            provider_id: '123123',
         });
 
         expect(appointment).toHaveProperty('id');
-        expect(appointment.provider_id).toBe('11234');
+        expect(appointment.provider_id).toBe('123123');
     });
 
     it('should not be able to create a two appointments on the same time', async () =>{
@@ -29,12 +30,14 @@ describe('CreateAppointment', ()=>{
 
         await createAppointment.execute({
             date: appointmentDate,
-            provider_id: '11234',
+            user_id: '123123',
+            provider_id: '123123',
         });
 
         await expect(createAppointment.execute({
             date: appointmentDate,
-            provider_id: '11234',
+            user_id: '123123',
+            provider_id: '123123',
         })).rejects.toBeInstanceOf(AppError);
     });
 });
--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -7,8 +7,9 @@ import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
-  date: Date;
   provider_id: string;
+  user_id: string;
+  date: Date;
 }
 
 @injectable()
@@ -18,7 +19,7 @@ class CreateAppointmentService {
     private appointmentsRepository: IAppointmentsRepository,
   ) {}
 
-  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
+  public async execute({ date, provider_id, user_id }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
@@ -31,6 +32,7 @@ class CreateAppointmentService {
 
     const appointment = await this.appointmentsRepository.create({
       provider_id,
+      user_id,
       date: appointmentDate,
     });
 
