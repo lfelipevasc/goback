946c9c35276e6f4944b5ed7c7cc6aa4ad85e806b
Luis Felipe <lfelipevasconcellos@hotmail.com>
2020-12-30 13:54:41 -0300

    upload files to Amazon S3

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -4,10 +4,11 @@ import uploadConfig from '@config/upload';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
-import IUsersRepository from '../repositories/IUsersRepository';
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
 
 import User from '../infra/typeorm/entities/User';
+
 interface IRequest {
   user_id: string;
   avatarFilename: string;
